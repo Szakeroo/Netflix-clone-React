257112fe3f5774b931558036bc92c34edfe2d292
szaker1990 <szaker1990@github.com>
2020-10-01 11:44:29 +0200

    Creating jumbotron

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Container, Inner, Wrapper, Title, SubTitle, Image, Item } from "./styles/jumbotron";
+
+export default function Jumbotron({children, direction = "row", ...restProps}) {
+    return (
+        <Item direction={direction}>
+            <Inner>
+                {children}
+            </Inner>
+        </Item>
+    )
+
+}
+
+Jumbotron.Container = function JumbotronContainer({children, ...restProps}) {
+    return <Container {...restProps}>{children}</Container>
+}
+Jumbotron.Wrapper = function JumbotronWrapper({children, ...restProps}) {
+    return <Wrapper {...restProps}>{children}</Wrapper>
+}
+Jumbotron.Title = function JumbotronTitle({children, ...restProps}) {
+    return <Title {...restProps}>{children}</Title>
+}
+Jumbotron.SubTitle = function JumbotronSubTitle({children, ...restProps}) {
+    return <SubTitle {...restProps}>{children}</SubTitle>
+}
+Jumbotron.Image = function JumbotronImage({...restProps}) {
+    return <Image {...restProps}/>
+}
