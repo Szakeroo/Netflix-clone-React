2faa73ec3f462ca108c7c5419f2f58a4d15a7e99
szaker1990 <szaker1990@github.com>
2020-10-05 17:45:32 +0200

    firebase data

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './app';
 import "normalize.css"
 import { GlobalStyles } from "./global-styles"
+import {firebase}  from "./lib/firebase.prod";
 
 ReactDOM.render(
     <>
