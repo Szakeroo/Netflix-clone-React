349e88de402eeb0032300a2984eb31633497fbe8
szaker1990 <szaker1990@github.com>
2020-10-05 18:02:40 +0200

    creating firebase context

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import { BrowserRouter as Router, Route} from "react-router-dom"
 import { Home, Browse, SignIn, SignUp} from "./pages";
 import * as ROUTES from "./constants/routes"
 
