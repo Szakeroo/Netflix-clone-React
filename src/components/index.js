634ad766edb8d2733a44bba185fafca3bc97bd0a
szaker1990 <szaker1990@github.com>
2020-10-06 11:27:24 +0200

    sign in page components

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,9 @@
 export { default as Accordion } from './accordion';
 export { default as Footer } from './footer';
 export { default as Jumbotron } from './jumbotron';
-export { default as Header } from "./header"
-export { default as EmailForm } from "./email-form"
-export { default as Feature } from "./feature"
+export { default as Header } from "./header";
+export { default as EmailForm } from "./email-form";
+export { default as Feature } from "./feature";
+export { default as Form } from "./form";
 
 
--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,5 +1,43 @@
-import React from "react";
+import React,{ useState } from "react";
+import { HeaderContainer } from "../containers/header";
+import { FooterContainer } from "../containers/footer";
+import { Form} from "../components";
 
 export default function SignIn() {
-    return<p>Hello from sign in</p>
+    const [emailAddress, setEmailAddress] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const isInValid = password === "" || emailAddress === "";
+    const handleSignIn = (e) => {
+        e.preventDefault()
+    }
+    return (
+        <>
+            <HeaderContainer>
+                <Form>
+                    <Form.Title>Sign In</Form.Title>
+                    {error && <Form.Error>{error}</Form.Error>}
+                    <Form.Base onSubmit={handleSignIn} method={"POST"}>
+                        <Form.Input
+                            placeholder={"Email Address"}
+                            value={emailAddress}
+                            onChange={({target}) => setEmailAddress(target.value)}
+                        />
+                        <Form.Input
+                            type={"password"}
+                            placeholder={"Password"}
+                            autoComplete={"off"}
+                            value={password}
+                            onChange={({target}) => setPassword(target.value)}
+                        />
+                        <Form.Submit disable={isInValid} type={"submit"}>
+                            Sign In
+                        </Form.Submit>
+                    </Form.Base>
+                </Form>
+            </HeaderContainer>
+            <FooterContainer/>
+        </>
+    );
 }
